12e02a3dcbd143a724e0116c1f3f486187322373
hiscaler <hiscaler@gmail.com>
2015-09-30 10:34:34 +0800

    添加 UtilHelper::isSerialized　和 UtilHelper::isSerializedString 函数，用于判断数据是否已经序列化和是否序列化字符

--- src/yadjet/helpers/UtilHelper.php
+++ src/yadjet/helpers/UtilHelper.php
@@ -61,4 +61,108 @@ class UtilHelper
         }
     }
 
+    /**
+     * Check value to find if it was serialized.   
+     *
+     * If $data is not an string, then returned value will always be false.
+     * Serialized data is always a string.
+     *
+     * @since 2.0.5
+     * @link https://core.trac.wordpress.org/browser/tags/4.3.1/src/wp-includes/functions.php WordPress
+     * 
+     * @param string $data   Value to check to see if was serialized.
+     * @param bool   $strict Optional. Whether to be strict about the end of the string. Default true.
+     * @return bool False if not serialized and true if it was.
+     */
+    public static function isSerialized($data, $strict = true)
+    {
+        // if it isn't a string, it isn't serialized.
+        if (!is_string($data)) {
+            return false;
+        }
+        $data = trim($data);
+        if ('N;' == $data) {
+            return true;
+        }
+        if (strlen($data) < 4) {
+            return false;
+        }
+        if (':' !== $data[1]) {
+            return false;
+        }
+        if ($strict) {
+            $lastc = substr($data, -1);
+            if (';' !== $lastc && '}' !== $lastc) {
+                return false;
+            }
+        } else {
+            $semicolon = strpos($data, ';');
+            $brace = strpos($data, '}');
+            // Either ; or } must exist.
+            if (false === $semicolon && false === $brace) {
+                return false;
+            }
+            // But neither must be in the first X characters.
+            if (false !== $semicolon && $semicolon < 3) {
+                return false;
+            }
+            if (false !== $brace && $brace < 4) {
+                return false;
+            }
+        }
+        $token = $data[0];
+        switch ($token) {
+            case 's' :
+                if ($strict) {
+                    if ('"' !== substr($data, -2, 1)) {
+                        return false;
+                    }
+                } elseif (false === strpos($data, '"')) {
+                    return false;
+                }
+            // or else fall through
+            case 'a' :
+            case 'O' :
+                return (bool) preg_match("/^{$token}:[0-9]+:/s", $data);
+            case 'b' :
+            case 'i' :
+            case 'd' :
+                $end = $strict ? '$' : '';
+                return (bool) preg_match("/^{$token}:[0-9.E-]+;$end/", $data);
+        }
+
+        return false;
+    }
+
+    /**
+     * Check whether serialized data is of string type.
+     *
+     * @since 2.0.5
+     * @link https://core.trac.wordpress.org/browser/tags/4.3.1/src/wp-includes/functions.php WordPress
+     *
+     * @param string $data Serialized data.
+     * @return bool False if not a serialized string, true if it is.
+     */
+    public static function isSerializedString($data)
+    {
+        // if it isn't a string, it isn't a serialized string.
+        if (!is_string($data)) {
+            return false;
+        }
+        $data = trim($data);
+        if (strlen($data) < 4) {
+            return false;
+        } elseif (':' !== $data[1]) {
+            return false;
+        } elseif (';' !== substr($data, -1)) {
+            return false;
+        } elseif ($data[0] !== 's') {
+            return false;
+        } elseif ('"' !== substr($data, -2, 1)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
