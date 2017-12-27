1c44b17f047e7442a5ce0811320bdca936575fb2
hiscaler <hiscaler@gmail.com>
2017-12-27 11:08:16 +0800

    ...

--- src/yadjet/helpers/StringHelper.php
+++ src/yadjet/helpers/StringHelper.php
@@ -2,8 +2,11 @@
 
 namespace yadjet\helpers;
 
+use Exception;
+
 /**
- * String Helper 
+ * String Helper
+ *
  * @author hiscaler <hiscaler@gmail.com>
  */
 class StringHelper
@@ -97,6 +100,7 @@ class StringHelper
 
     /**
      * 检测字符串是否为 UTF-8 编码
+     *
      * @param string $str
      * @return boolean
      */
@@ -161,6 +165,7 @@ class StringHelper
         $str = substr($str, $start, $length - $start);
 
         $end = mb_strpos($str, $endstr);
+
         return mb_substr($str, 0, $end);
     }
 
@@ -173,24 +178,26 @@ class StringHelper
             $encoded = mb_detect_encoding($data, $encode_arr);
             $data = mb_convert_encoding($data, $to, $encoded);
         }
+
         return $data;
     }
 
     /**
-      +----------------------------------------------------------
+     * +----------------------------------------------------------
      * 字符串截取，支持中文和其他编码
-      +----------------------------------------------------------
+     * +----------------------------------------------------------
+     *
      * @static
      * @access public
-      +----------------------------------------------------------
+     * +----------------------------------------------------------
      * @param string $str 需要转换的字符串
      * @param string $start 开始位置
      * @param string $length 截取长度
      * @param string $charset 编码格式
      * @param string $suffix 截断显示字符
-      +----------------------------------------------------------
+     * +----------------------------------------------------------
      * @return string
-      +----------------------------------------------------------
+    +----------------------------------------------------------
      */
     public static function msubStr($str, $start, $length, $charset = "utf-8", $suffix = '')
     {
@@ -209,38 +216,190 @@ class StringHelper
         return (empty($suffix)) ? $slice : $slice . $suffix;
     }
 
-    public static function generateRandomKey($length = 32)
+    /**
+     * Form Yii2 StringHelper class.
+     * Returns the number of bytes in the given string.
+     * This method ensures the string is treated as a byte array by using `mb_strlen()`.
+     *
+     * @param string $string the string being measured for length
+     * @return int the number of bytes in the given string.
+     */
+    public static function byteLength($string)
     {
-        if (!extension_loaded('mcrypt')) {
-            throw new InvalidConfigException('The mcrypt PHP extension is not installed.');
-        }
-        $bytes = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
-        if ($bytes === false) {
-            throw new Exception('Unable to generate random bytes.');
-        }
-
-        return $bytes;
+        return mb_strlen($string, '8bit');
     }
 
     /**
+     * From Yii2 Security class.
+     *
+     * Generates specified number of random bytes.
+     * Note that output may not be ASCII.
+     *
+     * @see generateRandomString() if you need a string.
+     *
+     * @param int $length the number of bytes to generate
+     * @return string the generated random bytes
+     * @throws InvalidParamException if wrong length is specified
+     * @throws Exception on failure.
+     */
+    public function generateRandomKey($length = 32, $useLibreSSL = null, $randomFile = null)
+    {
+        if (!is_int($length)) {
+            throw new Exception('First parameter ($length) must be an integer');
+        }
+
+        if ($length < 1) {
+            throw new Exception('First parameter ($length) must be greater than 0');
+        }
+
+        // always use random_bytes() if it is available
+        if (function_exists('random_bytes')) {
+            return random_bytes($length);
+        }
+
+        // The recent LibreSSL RNGs are faster and likely better than /dev/urandom.
+        // Parse OPENSSL_VERSION_TEXT because OPENSSL_VERSION_NUMBER is no use for LibreSSL.
+        // https://bugs.php.net/bug.php?id=71143
+        if ($useLibreSSL === null) {
+            $useLibreSSL = defined('OPENSSL_VERSION_TEXT')
+                && preg_match('{^LibreSSL (\d\d?)\.(\d\d?)\.(\d\d?)$}', OPENSSL_VERSION_TEXT, $matches)
+                && (10000 * $matches[1]) + (100 * $matches[2]) + $matches[3] >= 20105;
+        }
+
+        // Since 5.4.0, openssl_random_pseudo_bytes() reads from CryptGenRandom on Windows instead
+        // of using OpenSSL library. LibreSSL is OK everywhere but don't use OpenSSL on non-Windows.
+        if ($useLibreSSL
+            || (
+                DIRECTORY_SEPARATOR !== '/'
+                && substr_compare(PHP_OS, 'win', 0, 3, true) === 0
+                && function_exists('openssl_random_pseudo_bytes')
+            )
+        ) {
+            $key = openssl_random_pseudo_bytes($length, $cryptoStrong);
+            if ($cryptoStrong === false) {
+                throw new Exception(
+                    'openssl_random_pseudo_bytes() set $crypto_strong false. Your PHP setup is insecure.'
+                );
+            }
+            if ($key !== false && self::byteLength($key) === $length) {
+                return $key;
+            }
+        }
+
+        // mcrypt_create_iv() does not use libmcrypt. Since PHP 5.3.7 it directly reads
+        // CryptGenRandom on Windows. Elsewhere it directly reads /dev/urandom.
+        if (function_exists('mcrypt_create_iv')) {
+            $key = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
+            if (self::byteLength($key) === $length) {
+                return $key;
+            }
+        }
+
+        // If not on Windows, try to open a random device.
+        if ($randomFile === null && DIRECTORY_SEPARATOR === '/') {
+            // urandom is a symlink to random on FreeBSD.
+            $device = PHP_OS === 'FreeBSD' ? '/dev/random' : '/dev/urandom';
+            // Check random device for special character device protection mode. Use lstat()
+            // instead of stat() in case an attacker arranges a symlink to a fake device.
+            $lstat = @lstat($device);
+            if ($lstat !== false && ($lstat['mode'] & 0170000) === 020000) {
+                $randomFile = fopen($device, 'rb') ?: null;
+
+                if (is_resource($randomFile)) {
+                    // Reduce PHP stream buffer from default 8192 bytes to optimize data
+                    // transfer from the random device for smaller values of $length.
+                    // This also helps to keep future randoms out of user memory space.
+                    $bufferSize = 8;
+
+                    if (function_exists('stream_set_read_buffer')) {
+                        stream_set_read_buffer($randomFile, $bufferSize);
+                    }
+                    // stream_set_read_buffer() isn't implemented on HHVM
+                    if (function_exists('stream_set_chunk_size')) {
+                        stream_set_chunk_size($randomFile, $bufferSize);
+                    }
+                }
+            }
+        }
+
+        if (is_resource($randomFile)) {
+            $buffer = '';
+            $stillNeed = $length;
+            while ($stillNeed > 0) {
+                $someBytes = fread($randomFile, $stillNeed);
+                if ($someBytes === false) {
+                    break;
+                }
+                $buffer .= $someBytes;
+                $stillNeed -= self::byteLength($someBytes);
+                if ($stillNeed === 0) {
+                    // Leaving file pointer open in order to make next generation faster by reusing it.
+                    return $buffer;
+                }
+            }
+            fclose($randomFile);
+            $randomFile = null;
+        }
+
+        throw new Exception('Unable to generate a random key');
+    }
+
+    /**
+     * From Yii2 Security class.
      * Generates a random string of specified length.
      * The string generated matches [A-Za-z0-9_-]+ and is transparent to URL-encoding.
      *
-     * @param integer $length the length of the key in characters
-     * @throws Exception Exception on failure.
+     * @param int $length the length of the key in characters
      * @return string the generated random key
+     * @throws Exception on failure.
      */
-    public static function generateRandomString($length = 32)
+    public function generateRandomString($length = 32)
     {
-        $bytes = self::generateRandomKey($length);
-        // '=' character(s) returned by base64_encode() are always discarded because
-        // they are guaranteed to be after position $length in the base64_encode() output.
+        if (!is_int($length)) {
+            throw new Exception('First parameter ($length) must be an integer');
+        }
 
-        return strtr(substr(base64_encode($bytes), 0, $length), '+/', '_-');
+        if ($length < 1) {
+            throw new Exception('First parameter ($length) must be greater than 0');
+        }
+
+        $bytes = self::generateRandomKey($length);
+
+        return substr(self::base64UrlEncode($bytes), 0, $length);
+    }
+
+    /**
+     * Encodes string into "Base 64 Encoding with URL and Filename Safe Alphabet" (RFC 4648).
+     *
+     * > Note: Base 64 padding `=` may be at the end of the returned string.
+     * > `=` is not transparent to URL encoding.
+     *
+     * @see https://tools.ietf.org/html/rfc4648#page-7
+     * @param string $input the string to encode.
+     * @return string encoded string.
+     * @since 2.0.12
+     */
+    public static function base64UrlEncode($input)
+    {
+        return strtr(base64_encode($input), '+/', '-_');
+    }
+
+    /**
+     * Decodes "Base 64 Encoding with URL and Filename Safe Alphabet" (RFC 4648).
+     *
+     * @see https://tools.ietf.org/html/rfc4648#page-7
+     * @param string $input encoded string.
+     * @return string decoded string.
+     * @since 2.0.12
+     */
+    public static function base64UrlDecode($input)
+    {
+        return base64_decode(strtr($input, '-_', '+/'));
     }
 
     /**
      * 剔除字符串中的空格（包括中文和英文空格）
+     *
      * @param string $string
      * @return string
      */
@@ -283,6 +442,7 @@ class StringHelper
 
     /**
      * 生成 UUID
+     *
      * @return string
      */
     public static function uuid()
