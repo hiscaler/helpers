48924be03c19e36e72e9a6417e7f09b29769617a
hiscaler <hiscaler@gmail.com>
2018-10-10 12:43:45 +0800

    添加 UrlHelper 类，用于解析 url

---
+++ src/yadjet/helpers/UrlHelper.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace yadjet\helpers;
+
+/**
+ * URL Helper
+ *
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class UrlHelper
+{
+
+    /**
+     * 解析 url 参数
+     *
+     * @param $url
+     * @param $component
+     * @param null $default
+     * @return mixed|null
+     */
+    private static function _parse($url, $component, $default = null)
+    {
+        $res = parse_url(trim($url), $component);
+        if (!$res && $default) {
+            $res = $default;
+        }
+
+        return $res;
+    }
+
+    /**
+     * 获取 scheme
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function scheme($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_SCHEME, $default);
+    }
+
+    /**
+     * 获取 host
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function host($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_HOST, $default);
+    }
+
+    /**
+     * 获取 port
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function port($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_PORT, $default);
+    }
+
+    /**
+     * 获取 path
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function path($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_PATH, $default);
+    }
+
+    /**
+     * 获取 query
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function query($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_QUERY, $default);
+    }
+
+    /**
+     * 获取属有地址参数
+     *
+     * @param $url
+     * @param array $default
+     * @return array
+     */
+    public static function queries($url, $default = [])
+    {
+        $res = [];
+        $query = self::query($url);
+        if ($query) {
+            foreach (explode('&', $query) as $item) {
+                if (stripos($item, '=') === false) {
+                    $res[$item] = '';
+                    continue;
+                }
+                list($k, $v) = explode('=', $item);
+                $res[$k] = $v;
+            }
+        }
+
+        if ($default && is_array($default)) {
+            $res = array_merge($res, $default);
+        }
+
+        return $res;
+    }
+
+    /**
+     * 根据 $key 获取地址中的值
+     *
+     * @param $url
+     * @param $key
+     * @param null $default
+     * @return null
+     */
+    public static function findQueryByKey($url, $key, $default = null)
+    {
+        $res = null;
+        $query = self::query($url);
+        if ($query) {
+            $key = strtolower($key);
+            foreach (explode('&', $query) as $item) {
+                $items = explode('=', $item);
+                if (strtolower($items[0]) == $key) {
+                    $res = isset($items[1]) ? $items[1] : '';
+                    break;
+                }
+            }
+        }
+
+        return $res ?: $default;
+    }
+
+    /**
+     * 获取用户名
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function username($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_USER, $default);
+    }
+
+    /**
+     * 获取密码
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function password($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_PASS, $default);
+    }
+
+    /**
+     * 获取锚点
+     *
+     * @param $url
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function fragment($url, $default = null)
+    {
+        return self::_parse($url, PHP_URL_FRAGMENT, $default);
+    }
+
+}
---
+++ tests/UrlHelperTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace yadjet\helpers;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UrlHelperTest
+ *
+ * @package yadjet\helpers
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class UrlHelperTest extends TestCase
+{
+
+    public function testScheme()
+    {
+        $this->assertEquals(UrlHelper::scheme('http://www.example.com'), 'http');
+        $this->assertEquals(UrlHelper::scheme('https://www.example.com'), 'https');
+        $this->assertEquals(UrlHelper::scheme('//www.example.com'), '');
+        $this->assertEquals(UrlHelper::scheme('//www.example.com', 'http'), 'http');
+        $this->assertEquals(UrlHelper::scheme('www.example.com'), '');
+        $this->assertEquals(UrlHelper::scheme('www.example.com', 'http'), 'http');
+    }
+
+    public function testHost()
+    {
+        $this->assertEquals(UrlHelper::host('http://www.example.com'), 'www.example.com');
+        $this->assertEquals(UrlHelper::host('https://www.example.com'), 'www.example.com');
+        $this->assertEquals(UrlHelper::host('//www.example.com'), 'www.example.com');
+        $this->assertEquals(UrlHelper::host('www.example.com'), '');
+    }
+
+    public function testPort()
+    {
+        $this->assertEquals(UrlHelper::port('http://www.example.com'), null);
+        $this->assertEquals(UrlHelper::port('https://www.example.com'), null);
+        $this->assertEquals(UrlHelper::port('//www.example.com:80'), '80');
+        $this->assertEquals(UrlHelper::port('www.example.com:80'), 80);
+    }
+
+    public function testFindQueryByKey()
+    {
+        $this->assertEquals(UrlHelper::findQueryByKey('http://www.example.com/a.html?a=1&b=2', 'a'), '1');
+        $this->assertEquals(UrlHelper::findQueryByKey('http://www.example.com/a.html?a=1&b=2&c', 'aa'), null);
+        $this->assertEquals(UrlHelper::findQueryByKey('http://www.example.com/a.html?a=1&b=2&c', 'aa', '1'), '1');
+        $this->assertEquals(UrlHelper::findQueryByKey('http://www.example.com/a.html?a=1&b=2&c', 'c'), '');
+    }
+
+    public function testQueries()
+    {
+        $this->assertSame(UrlHelper::queries('http://www.example.com/a.html?a=1&b=2'), ['a' => '1', 'b' => '2']);
+        $this->assertSame(UrlHelper::queries('http://www.example.com/a.html?a=1&b=2&c'), ['a' => '1', 'b' => '2', 'c' => '']);
+        $this->assertSame(UrlHelper::queries('http://www.example.com/a.html?a=1&b=2&c', ['d' => 5]), ['a' => '1', 'b' => '2', 'c' => '', 'd' => 5]);
+    }
+
+}
